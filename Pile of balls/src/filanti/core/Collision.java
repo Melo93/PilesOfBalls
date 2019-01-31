3e46a3dddcadff249a7dc46dfd9394fbc98e04c9
cicci <cicci@192.168.1.27>
2019-01-31 18:20:28 +0100

    REFRESH

--- Pile of balls/src/filanti/core/Collision.java
+++
@@ -1,5 +0,0 @@
-package filanti.core;
-
-public enum Collision {
-	Left, Right
-}
--- Pile of balls/src/filanti/gui/GameMain.java
+++
@@ -1,8 +0,0 @@
-package filanti.gui;
-
-public class GameMain {
-	public static void main(String[] args) {
-		@SuppressWarnings("unused")
-		GameFrame gameFrame = new GameFrame(); // creo il frame
-	}
-}
